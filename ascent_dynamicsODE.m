83f4b82094af16f359a22daae47ba80f8caa9670
Robin <rgj.duprat@gmail.com>
2021-09-16 14:53:19 +0200

    rename of the ascent_dynamics file and first equation
    ascent_dynamics -> ascent_dynamicsODE
    
    code for differential equations

---
+++ ascent_dynamicsODE.m
@@ -0,0 +1,25 @@
+function [dy] = ascent_dynamicsODE(t, tf, T, D, y)
+%ASCENT_DYNAMICS: This function provide the differential equations for the
+%dynamics of the rockets during the ascention phase.
+%   The function return a column vector dy corresponding to [acceleration,
+%   flight path angle, ground distance rate from lift-off, altitude
+%   rate, mass flow rate].
+%   Units: [a = m/s^2, dgamma = 1/s, dh = m/s, dx = m/s, dm = kg/s]. 
+%   We assume that%   the rocket doesn't generate lift. 
+%   Forces at stake: thrust T, drag D, weight m*g.
+%   y is the state vector, dy is the time derivative of y
+
+Re = 6371e3; %Earth radius (m). To adapt with the Launch point (average 6371km)
+g0=9.81; %gravitational acceleration on Earth surface (m/s^2)
+%%TBD: Isp
+
+dy = zeros(5,1);
+
+dy(1) = (T-D)/y(5) - g*sin(y(2)); %acceleration (m/s^2)
+dy(2) = -1/y(1) * (g-(y(1)^2)/(Re+y(4))); %flight path angle (1/s)
+dy(3) = Re*y(1)*cos(y(2))/(Re+y(4)); %ground distance rate (m/s)
+dy(4) = y(1)*sin(y(2)); %altitude rate (m/s)
+dy(5) = -abs(T)/Isp/g0; %mass flow rate (kg/s)
+
+end
+
