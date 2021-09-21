d9e0d02062f158a14b9b7631f0624ea1026043bc
Robin <rgj.duprat@gmail.com>
2021-09-21 15:02:06 +0200

    Last phases of the gravity turn
    We still need to deal with the final conditions and we have a problem about a vector dimension

--- ascent_dynamicsODE.m
+++ ascent_dynamicsODE.m
@@ -1,4 +1,4 @@
-function [dy] = ascent_dynamicsODE(T, y, param)
+function dy = ascent_dynamicsODE(T, y, param)
 %ASCENT_DYNAMICS: This function provide the differential equations for the
 %dynamics of the rockets during the ascention phase.
 %   The function return a column vector dy corresponding to [acceleration,
@@ -12,21 +12,26 @@ function [dy] = ascent_dynamicsODE(T, y, param)
 %% Earth parameters
 Re = 6371e3; %Earth radius (m). To adapt with the Launch point (average 6371km)
 g0=9.80665; %gravitational acceleration on Earth at sea level (m/s^2)
+mu_E = 3.986e14; %Earth's gravitationnal constant (m^3/s^2)
 
 %% Rocket parameters
 Isp = param(1); %Isp (s). TBD
 Cd = param(2); %Drag coefficient. 1st assumption: the rocket is a cylinder (cf. Wikipedia Drag Coefficient)
 A = param(3); %Surface of the rocket in contact with the airflow (m^2)
-
+stage = param(4);
 %% Rocket dynamaic equation in the Troposphere
 dy = zeros(5,1);
-g = g0*(y(4)/(Re+y(4)))^2; %Earth model: gravitational accelration in function of the alitude
+g = mu_E/((Re+y(3))^2); %Earth model: gravitational accelration in function of the alitude
 
-rho = atmosphere(y(3));
+rho = atmosphere(y(3), g);
 D = 0.5*A*rho*Cd*y(1)^2; % Drag (N)
 
 dy(1) = (T-D)/y(5) - g*sin(y(2)); %acceleration (m/s^2)
-dy(2) = -1/y(1) * (g-(y(1)^2)/(Re+y(4)))*cos(y(2)); %flight path angle (1/s)
+if stage ~= 3
+    dy(2) = -1/y(1) * (g-(y(1)^2)/(Re+y(4)))*cos(y(2)); %flight path angle (1/s)
+else
+    dy(2) = 0; %in this phase, gamma is no longer a variable because of steering law
+end
 dy(3) = Re*y(1)*cos(y(2))/(Re+y(4)); %ground distance rate (m/s)
 dy(4) = y(1)*sin(y(2)); %altitude rate (m/s)
 dy(5) = -abs(T)/Isp/g0; %mass flow rate (kg/s)
--- main_reusable_rocket.m
+++ main_reusable_rocket.m
@@ -1,5 +1,6 @@
 %% Description
 % Gravity turn (cf. SD2900 - Rocket Dynamics, slide 25)
+    %Assuming Lift = 0
     %5 phases:
     % 1. Let the 1st stage go vertical for a short time to pick up speed to
     % avoid a too large dgamma/dt
@@ -20,20 +21,25 @@ mu_E=3.968e14; % gravitational parameter of Earth (m^3s^-2)
 Re=6378e3; % mean radius of Earth (m)
 g0=9.80665; % gravity of Earth at sea level (m/s^2)
 
-m0 = 1000e3; % initiale mass of the rocket (kg)
-t0 = O; % ignition time of the 1st stage (s)
+t0 = 0; % ignition time of the 1st stage (s)
 
+options = odeset('RelTol',1e-10,'AbsTol',1e-9);
 %% Rocket parameters
-Isp = [400, 467, 400]; %Isp (s) for 1st stage. TBD
+Isp = [400, 400, 467]; %Isp (s) for 1st stage. TBD
 Cd = 0.85; %Drag coefficient. 1st assumption: the rocket is a cylinder (cf. Wikipedia Drag Coefficient)
 A = 1; %Surface of the rocket in contact with the airflow (m^2)
 
-T = [11e6, 11e6]; %stages trhrust (N)
+T = [11e6, 11e6, 180e3]; %stages' trhrust (N)
+ms = [100, 100, 100]; %stages' strucutal mass (kg)
+mp = [500, 500, 500]; %stages' propellant mass (kg)
 
+m_p = 732.8; %Mass of the payload at launch (kg)
+m0 = sum(ms) + sum(mp) + m_p; %Total mass of the rocket at lift-off (kg)
 %% Phases
 
 % 1.
 % Initial condition
+stage = 1; %Stage currently use by the rocket
 V = 0; % (m/s)
 gamma = 0; % (rad)
 x = 0; % (m)
@@ -41,16 +47,36 @@ h = 0; % (m) Sea level, to adpat in function of the sarting point
 m = m0;
 y0 = [V; gamma; x; h; m]; % State vector, 1 column
 
-tf1 = 10; % Final time for phase 1 (s). Must be a short time
+ti1 = 10; % Final time for phase 1 (s). Must be a short time (i for intermediate)
 % On a paper we got 12s.
-param = [Isp(1), Cd, A];
+param = [Isp(stage), Cd, A, stage];
 %We need to estimate the thrust delivered by the engines
-[t, y] = ode45(@(t, y) ascent_dynamicsODE(T(1), y0, param), [to, tf1], y0);
+[t, y] = ode45(@(t, y) ascent_dynamicsODE(T(stage), y, param), [t0, ti1], y0, options);
 
 %2.
 gamma = 0.05*pi/180; % (rad) Non-zero value to start gravity turn
-y1 = y;
-y1(2) = gamma;
-tb1 = mp*g0*Isp/T1; %burnout time of 1st stage (s)
+y01 = y;
+y01(2) = gamma;
+tb1 = mp(stage)*g0*Isp(stage)/T(stage); %burnout time of 1st stage (s)
+tf1 = tb1 - ti1; %end of the 1st stage phase
+[t1, y1] = ode45(@(t, y) ascent_dynamicsODE(T(stage), y, param), [ti1, tf1], y01, options);
 
-[t, y] = ode45(@(t, y) ascent_dynamicsODE(T(2), y1, param), [tf1, tb1-tf1], y1);
+%3.
+stage = 2;
+y02 = y;
+y02(5) = y02(5)-ms(stage-1); %1st stage removal
+param(1) = Isp(stage); %Isp update for 2nd stage
+param(5) = stage;
+tb2 = mp(stage)*g0*Isp/T(stage); %burnout time of 2nd stage (s)
+tf2 = tb2 - tf1; %end of the 2nd stage phase
+[t2, y2] = ode45(@(t, y) ascent_dynamicsODE(T(stage), y, param), [tf1, tf2], y02, options);
+
+%4.
+stage = 3;
+y03 = y;
+y03(5) = y03(5)-ms(stage-1); %2nd stage removal
+param(1) = Isp(stage); %Isp update for 2nd stage
+param(5) = stage;
+tb3 = mp(stage)*g0*Isp(stage)/T(stage); %burnout time of 2nd stage (s)
+tf3 = tb3 - tf2; %end of the 2nd stage phase
+[t3, y3] = ode45(@(t, y) ascent_dynamicsODE(T(stage), y, param), [tf2, tf3], y03, options);
