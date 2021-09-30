c2f0f3bf6afc3c8f2d2ea14f1b0501af21196810
Robin <rgj.duprat@gmail.com>
2021-09-30 12:04:50 +0200

    ploting stuff

--- main_expendable_rocket.m
+++ main_expendable_rocket.m
@@ -14,7 +14,7 @@
     % the correct altitude. Note that gamma is NOT a variable for 3rd stage is
     % used.
     % Final conditions (for the rocket and not the mission): gamma=0, H=H*, V=V*
-
+close all
 %% Constant values
 
 mu_E=3.968e14; % gravitational parameter of Earth (m^3s^-2)
@@ -31,11 +31,11 @@ iV = 1; igamma = 2; ih = 3; ix = 4; im = 5;
 Isp = [378, 359, 467]; %Isp (s) for 1st stage. TBD
 Cd = 0.85; %Drag coefficient. 1st assumption: the rocket is a cylinder (cf. Wikipedia Drag Coefficient)
 A = pi*3.66^2; %Surface of the rocket in contact with the airflow (m^2)
-nb_engines = [3, 1, 1];
+nb_engines = [1, 1, 1];
 T = [2205000, 533000, 180000].*nb_engines; %stages' trhrust (N)
 ms = [16e3, 4e3, 1.5e3]; %stages' strucutal mass (kg)
-mp = [418647.6, 31134, 3751.57]; %stages' propellant mass (kg)
-m_p = 732.8; %Mass of the payload at launch (kg)
+mp = [96255.4, 16013.9, 3751.57]; %stages' propellant mass (kg)
+m_p = 732.8 + 1.6927e3; %Mass of the payload at launch (kg) + Fuel for orbital maneuvers
 m0 = sum(ms) + sum(mp) + m_p; %Total mass of the rocket at lift-off (kg)
 
 V0 = 0; % (m/s)
@@ -65,7 +65,7 @@ y02 = [y1(end,iV) y1(end,igamma) y1(end,ih) y1(end,ix) y1(end,im)];
 
 tb1 = mp(stage)*g0*Isp(stage)/T(stage); %burnout time of 1st stage (s)
 tf1 = tb1; %end of the 1st stage phase
-[t2, y2] = ode45(@(t, y) ascent_dynamicsODE(t, T(stage), y, param), [ti1 tf1], y02, options);
+[t2, y2] = ode45(@(t, y) ascent_dynamicsODE(t, T(stage), y, param, [y1(end,igamma), 32*pi/180], [ti1 tf1]), [ti1 tf1], y02, options);
 
 %3.
 phase = 3;
@@ -77,7 +77,7 @@ y03(end,im) = y03(end,im)-ms(stage-1); %1st stage removal
 
 tb2 = mp(stage)*g0*Isp(stage)/T(stage); %burnout time of 2nd stage (s)
 tf2 = tb2 + tf1; %end of the 2nd stage phase
-[t3, y3] = ode45(@(t, y) ascent_dynamicsODE(t, T(stage), y, param), [tf1 tf2], y03, options);
+[t3, y3] = ode45(@(t, y) ascent_dynamicsODE(t, T(stage), y, param, [y2(end,igamma), 2*pi/180], [tf1 tf2]), [tf1 tf2], y03, options);
 
 %4.
 phase = 4;
@@ -94,15 +94,48 @@ for i=1:size(y4(:,2),1)
     y4(i,igamma) = atan(tan(y3(end,igamma)*(1-(t4(i)-tf2)/tb3))); %Steering law: linear tangent law
 end
 
+% 6. Hohmann transfer : using 3rd stage
+%Re=6378e3; % mean radius of Earth (m)
+h= Re + y4(end,ih); % Assuming a parking orbit of 200km.
+%mu = 3.986004418e14; % constant, assuming M+m is approximately M and constant
+h_target = 29599.8e3; % target orbit
+m_init=y4(end,im); %initial mass for phase 6. 2100kg for the moment, will be y(5) in the end
+
+%m_star=732.8;
+
+%g0=9.80665; 
+
+Vc1 = sqrt(mu_E/h);% current speed on parking orbit
+Vc2 = sqrt(mu_E/h_target); % to be speed on MEO
+a = (h+h_target)/2; %semi major axis of the transfer orbit
+V_perigee = sqrt(mu_E*(2/h-1/a)); %velocity at the perigee of the transfer orbit 
+V_apogee = sqrt(mu_E*(2/h_target-1/a)); %velocity at the apogee of the transfer orbit
+Delta_V_perigee = V_perigee - Vc1;
+Delta_V_apogee = Vc2 - V_apogee;
+Delta_V = Delta_V_perigee + Delta_V_apogee; % cost of the total transfer 
+Delta_t = pi*sqrt(a^3/mu_E); % transfer time
+
+Delta_m = m_init-m_init*exp(-Delta_V/(Isp(stage)*g0));
+disp(m_init - Delta_m);
+m_s=Delta_m-m_p; %in general m_s = 1/7 m_p
+
 %% Ploting phase
+lengend_list=["Ascent phase before gravity turn",...
+    "Thrusting with stage 1",...
+    "Thrusting with stage 2",...
+    "Thrusting with stage 3"] ;
+font_size = 26;
 figure(1); hold on;
+set(0,'defaultTextFontName', 'Times')
 plot(t1,y1(:,ih)/1e3,'r','LineWidth',2);
 plot(t2,y2(:,ih)/1e3,'g','LineWidth',2);
 plot(t3,y3(:,ih)/1e3,'b','LineWidth',2);
 plot(t4,y4(:,ih)/1e3,'y','LineWidth',2);
-title('Altitude change');
+title('Expendable rocket: Altitude variation in function of time');
 xlabel('Time (s)');
 ylabel('Altitude (km)');
+set(findall(gcf,'-property','FontSize'),'FontSize',font_size, 'FontName', "Times New Roman");
+legend(lengend_list);
 grid;
 
 figure(2); hold on;
@@ -110,9 +143,11 @@ plot(t1,y1(:,igamma)*180/pi,'r','LineWidth',2);
 plot(t2,y2(:,igamma)*180/pi,'g','LineWidth',2);
 plot(t3,y3(:,igamma)*180/pi,'b','LineWidth',2);
 plot(t4,y4(:,igamma)*180/pi,'y','LineWidth',2);
-title('Flight path angle change');
+title('Expendable rocket: Flight path angle variation in function of time');
 xlabel('Time (s)');
 ylabel('Flight path angle (deg)');
+set(findall(gcf,'-property','FontSize'),'FontSize',font_size, 'FontName', "Times New Roman");
+legend(lengend_list);
 grid;
 
 figure(3); hold on;
@@ -120,9 +155,11 @@ plot(y1(:,ix)/1e3,y1(:,ih)/1e3,'r','LineWidth',2);
 plot(y2(:,ix)/1e3,y2(:,ih)/1e3,'g','LineWidth',2);
 plot(y3(:,ix)/1e3,y3(:,ih)/1e3,'b','LineWidth',2);
 plot(y4(:,ix)/1e3,y4(:,ih)/1e3,'y','LineWidth',2);
-title('Altitude change');
+title('Expendable rocket: Vertical profile');
 xlabel('X position (km)');
 ylabel('Altitude (km)');
+set(findall(gcf,'-property','FontSize'),'FontSize',font_size, 'FontName', "Times New Roman");
+legend(lengend_list);
 grid;
 
 figure(4); hold on;
@@ -130,7 +167,9 @@ plot(t1,y1(:,iV)/1e3,'r','LineWidth',2);
 plot(t2,y2(:,iV)/1e3,'g','LineWidth',2);
 plot(t3,y3(:,iV)/1e3,'b','LineWidth',2);
 plot(t4,y4(:,iV)/1e3,'y','LineWidth',2);
-title('Speed change');
+title('Expendable rocket: Velocity variation in function of time');
 xlabel('Time (s)');
 ylabel('Speed (km/s)');
+set(findall(gcf,'-property','FontSize'),'FontSize',font_size, 'FontName', "Times New Roman");
+legend(lengend_list);
 grid;
